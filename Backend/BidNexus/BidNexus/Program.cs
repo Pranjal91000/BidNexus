d0317b217dce4b0134d6b52f303285055efc15b0
Pranjal Singh <95901436+Pranjal91000@users.noreply.github.com>
2026-09-23 11:16:02 +0530

    refactor(globaldata): register application services

--- Backend/BidNexus/BidNexus/Program.cs
+++ Backend/BidNexus/BidNexus/Program.cs
@@ -1,34 +1,27 @@
+using API.Abstraction.GlobalData;
+using API.Services.GlobalData;
 using Infrastructure;
+using Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
-// Add services to the container.
-
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddInfrastructure();
+builder.Services.AddScoped<IGlobalDataService, GlobalDataService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
