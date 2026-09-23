f7ffca46f5c0aaf37628203fcfe0e020a2dba235
Pranjal Singh <95901436+Pranjal91000@users.noreply.github.com>
2026-09-23 11:16:00 +0530

    refactor(globaldata): register repository dependencies

--- Backend/BidNexus/Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ Backend/BidNexus/Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Core.Abstraction.GlobalData;
+using Infrastructure.Repository.GlobalData;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Infrastructure.Extensions
+namespace Infrastructure.Extensions;
+
+public static class ServiceCollectionExtension
 {
-    public static class ServiceCollectionExtension
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
-        {
-            return services;
-        }
+        services.AddScoped<IGlobalDataRepository, GlobalDataRepository>();
 
+        return services;
     }
 }
